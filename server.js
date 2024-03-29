79960dc2275d44a5ee6e3c902fdda958b79bdd86
Elkin Kristan <scyke1@nottingham.edu.cn>
2024-03-29 22:29:22 +0800

    Added POST request function

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 const port = 5000;
 
-const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const { nearestNeighbors} = require('./recommendation.js');
 
-app.get('/', (req, res) => {
-    res.send("Hello World");
+app.post('/api/formdata', (req, res) => { 
+
+    console.log(req.body);
+
+    res.send('Recieved data');
 })
 
-app.get('/api/recommended_items', (req, res) => {
-    res.json(nearestNeighbors);
-})
+// app.get('/', (req, res) => {
+//     res.send("Hello World");
+// })
+
+// app.get('/api/recommended_items', (req, res) => {
+//     res.json(nearestNeighbors);
+// })
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
